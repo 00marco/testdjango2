c6a50ec5340cfe49da68de3aad1b7073dbfc700c
Marco <marcopulido00@gmail.com>
2020-06-16 01:41:33 +0800

    add rest of repo

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/guest/Documents/work/test_django_rest/django_heroku_test/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
